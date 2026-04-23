69404a34841927c6b017bde8745e58c9a604cbe3
ErwinZorin <gza1275su@gmail.com>
2026-04-23 13:32:50 +0300

    Create task_4-13-5.py

---
+++ projects 4/task_4-13/task_4-13-5.py
@@ -0,0 +1,9 @@
+n = int(input("Количество чисел: "))
+max_value = 0
+i = 2
+while i <= n:
+    x = float(input(f"Введите число {i}: "))
+    if x > ma:
+        m = x
+    i = i + 1
+print("Максимальное число:", m)
